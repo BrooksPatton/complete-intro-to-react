b56b503f5b343aecf6ab4be535dac175ca84d994
Brooks Patton <brooks@brookzerker.com>
2016-10-03 19:46:37 -0600

    set up prop requirements in header

--- js/Header.jsx
+++ js/Header.jsx
@@ -1,8 +1,27 @@
 const React = require('react')
 const {Link} = require('react-router')
+const {func, bool, string} = React.PropTypes
 
 const Header = React.createClass({
+  propTypes: {
+    handleSearchTerm: func,
+    showSearch: bool,
+    searchTerm: string
+  },
+
   render () {
+    let utilSpace
+
+    if (this.props.showSearch) {
+      utilSpace = <input className='search-input' type='text' placeholder='Search' value={this.props.searchTerm} onChange={this.props.handleSearchTerm} />
+    } else {
+      utilSpace = (
+        <h2 className='header-back'>
+          <Link to='/search'>Back</Link>
+        </h2>
+      )
+    }
+
     return (
       <header className='header'>
         <h1 className='brand'>
@@ -10,9 +29,7 @@ const Header = React.createClass({
             svideo
           </Link>
         </h1>
-        <h2 className='header-back'>
-          <Link to='/search'>Back</Link>
-        </h2>
+        {utilSpace}
       </header>
     )
   }
