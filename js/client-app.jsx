3cd063cb2cfc30009e26f9c61a84b274477e19f5
Brooks Patton <brooks@brookzerker.com>
2016-10-05 10:41:36 -0600

    outsourced start of app to browserentry

--- js/client-app.jsx
+++ js/client-app.jsx
@@ -1,5 +1,4 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
 const Landing = require('./landing')
 const {Router, Route, hashHistory, IndexRoute} = require('react-router')
 const Search = require('./search')
@@ -24,4 +23,4 @@ const App = React.createClass({
   }
 })
 
-ReactDOM.render(<App />, document.getElementById('app'))
+module.exports = App
