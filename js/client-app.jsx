67bd448a85bb5d653f564a8695c5025f65bc8c25
Brooks Patton <brooks@brookzerker.com>
2016-10-05 10:49:09 -0600

    browser vs. server rendering

--- js/client-app.jsx
+++ js/client-app.jsx
@@ -1,26 +1,32 @@
 const React = require('react')
 const Landing = require('./landing')
-const {Router, Route, hashHistory, IndexRoute} = require('react-router')
+const {Router, Route, browserHistory, IndexRoute} = require('react-router')
 const Search = require('./search')
 const Layout = require('./layout')
 const Details = require('./Details')
 const {store} = require('./Store')
 const {Provider} = require('react-redux')
 
+const myRoutes = () => {
+  <Route path='/' component={Layout}>
+    <IndexRoute component={Landing} />
+    <Route path='/search' component={Search} />
+    <Route path='/details/:id' component={Details} />
+  </Route>
+}
+
 const App = React.createClass({
   render () {
     return (
       <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={Details} />
-          </Route>
+        <Router history={browserHistory}>
+          {myRoutes()}
         </Router>
       </Provider>
     )
   }
 })
+
+App.routes = myRoutes
 
 module.exports = App
